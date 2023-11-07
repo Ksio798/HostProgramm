c2e798c36f3b22a3385df4c0d71f9352b6788f4f
Ksio798 <66674663+Ksio798@users.noreply.github.com>
2023-11-07 20:37:01 +1000

    Initial commit

---
+++ MainController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace Host
+{
+    internal class MainController
+    {
+        public MainWindow window;       
+        TcpListener server = null;
+        Int32 port = 8888;
+
+        public MainController(MainWindow w)
+        {          
+            window = w;
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            SetHostName();
+            Task task = Task.Factory.StartNew(Connect);
+        }
+
+        void Connect()
+        {
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Task task = Task.Factory.StartNew(() => { HandleClient(client); });
+            }
+        }
+
+        private void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            while (client != null)
+            {
+                try
+                {
+                    NetworkStream stream = client.GetStream();
+                    StreamReader reader = new StreamReader(stream);
+                    string request = reader.ReadLine();
+                    window.Dispatcher.Invoke(() => { window.UpdateLabel(request); });
+                }
+                catch { break; }
+            }
+
+            window.Dispatcher.Invoke(() => { window.UpdateLabel("end"); });
+
+            client.Close();
+        }      
+        
+        void SetHostName()
+        {
+            window.SetName(Dns.GetHostName());
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Host
+{
+    public partial class MainWindow : Window
+    {
+        MainController mainControler;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainControler = new MainController(this);       
+        }
+
+        public void UpdateLabel(string text)
+        {
+            textPanel.Text = text;
+            image.Source = new BitmapImage(new Uri("/Images/" + text + ".jpg", UriKind.Relative));
+        }
+
+        public void SetName(string text)
+        {
+            Title = "Host: " + text;
+        }
+    }
+}
